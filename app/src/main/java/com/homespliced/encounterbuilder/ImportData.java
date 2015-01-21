fc13ec5de4281cc48420dc187ba58f2ef0883a77
CrypticSplicer <splicer@homespliced.com>
2015-01-21 03:19:50 -0500

    Got most of CSV to SQL insertion working.

--- app/src/main/java/com/homespliced/encounterbuilder/ImportData.java
+++ app/src/main/java/com/homespliced/encounterbuilder/ImportData.java
@@ -1,16 +1,43 @@
 package com.homespliced.encounterbuilder;
 
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
- * Created by Keith on 12/25/2014.
+ * Created by Keith on 1/21/2015.
  */
-public class ImportData {
+public class ImportData extends MonsterStatDatabaseHelper{
+    private Context context;
 
-    public static void Import(String fileLocation) {
-        new File(fileLocation);
+    public ImportData(MainActivity context) {
+        super(context);
+    }
+
+    public boolean ImportMonster(String fileLocation) {
+        try {
+            BufferedReader CSVImport = new BufferedReader(new FileReader(fileLocation));
+
+            String newLine;
+            while ((newLine = CSVImport.readLine()) != null) {
+                String[] lineParts = newLine.split(",");
+
+                //TODO- ERROR CHECK STRING ARRAY BEFORE SENDING TO MHELPER
+                dropTable("MonsterStatTable");
+                addMonsterLevel(lineParts);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 }
-
